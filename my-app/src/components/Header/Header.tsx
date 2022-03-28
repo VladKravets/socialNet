8623878127682a82ee2d212a71c087e36ad5b429
Vlad Kravets <hazardvlad19@gmail.com>
2022-03-28 16:17:01 +0300

    ' fix styling project '

--- my-app/src/components/Header/Header.tsx
+++ my-app/src/components/Header/Header.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import s from './Header.module.css'
 
 
+
 const Header = () => {
     return (
         <header className={s.header}>
-            <div className={`${s.content} ${s.left}`}></div>
-            {/*<img src="https://freepikpsd.com/file/2020/03/all-network-logo-png-.png" alt="logo-socialNetwork"/>*/}
-            <span className={s.title}>.forEach</span>
+           <div className={s.title}>.forEach()</div>
         </header>
     );
 };
 export default Header;
+
+
+
+
+
--- my-app/src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ my-app/src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -7,7 +7,7 @@ const ProfileInfo = () => {
     return (
         <div>
             <div>
-                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt=""/>
+                <img src="https://media.istockphoto.com/photos/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-picture-id825383494?k=20&m=825383494&s=612x612&w=0&h=tEqZ5HFZcM3lmDm_cmI7hOeceiqy9gYrkyLTTkrXdY4=" alt=""/>
             </div>
             <div className={s.descriptionBlock}>
                 ava+description
