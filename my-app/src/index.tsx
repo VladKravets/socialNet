65e2180053a96ba5c6613c6b1cc94d23e0572516
Vlad Kravets <hazardvlad19@gmail.com>
2022-03-29 12:48:23 +0300

    'fix render'

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -4,19 +4,8 @@ import './index.css';
 import App from './App';
 import {store} from "./Redux/redux-store";
 import {Provider} from "react-redux";
-import {RootStateType} from "./Redux/state";
 
-export let rerenderEntireTree = (state: RootStateType) => {
-    ReactDOM.render(
-        <Provider store={store}>
-            <App/>
-        </Provider>, document.getElementById('root'));
-}
-
-
-rerenderEntireTree(store.getState())
-
-store.subscribe(() => {
-    let state = store.getState()
-    rerenderEntireTree(state)
-})
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root'));
