d2836bc34af487926f16092805771e4ab31ca7aa
Vlad Kravets <hazardvlad19@gmail.com>
2022-03-30 11:35:37 +0300

    'added a container component from Users'

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,7 +8,7 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import {Music} from "./components/Music";
 import {News} from "./components/News";
 import {Settings} from "./components/Settings";
-import {Users} from "./components/Users/Users";
+import UsersContainer from "./components/Users/UsersContainer";
 
 
 const App = () => {
@@ -23,7 +23,7 @@ const App = () => {
                             <Route path={'/dialogs/*'} element={<DialogsContainer/>}/>
                             <Route path={':id'} element={<DialogsContainer/>}/>
                             <Route path={'/profile/*'} element={<Profile/>}/>
-                            <Route path={'/users/*'} element={<Users/>}/>
+                            <Route path={'/users/*'} element={<UsersContainer/>}/>
                             <Route path={'/music'} element={<Music/>}/>
                             <Route path={'/news'} element={<News/>}/>
                             <Route path={'/settings'} element={<Settings/>}/>
---
+++ my-app/src/components/Users/UsersContainer.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {connect} from "react-redux";
+import {Users} from "./Users";
+import {RootStateType} from "../../Redux/redux-store";
+import {Dispatch} from "redux";
+import {followAC, setUsersAC, unfolllowAC, UsersType} from "../../Redux/users-reducer";
+
+type MapStateToPropsType={
+    users:UsersType[]
+}
+type MapStateToDispatchType={
+follow:(userID:number)=>void
+unfollow:(userID:number)=>void
+setUsers:(users:UsersType[])=>void
+}
+
+let mapStateToProps = (state: RootStateType):MapStateToPropsType => {
+    return {
+        users: state.usersPage.users
+    }
+}
+let mapStateToDispatch = (dispatch: Dispatch):MapStateToDispatchType => {
+    return {
+        follow: (userID) => {
+            dispatch(followAC(userID))
+        },
+        unfollow: (userID) => {
+            dispatch(unfolllowAC(userID))
+        },
+        setUsers: (users) => {
+            dispatch(setUsersAC(users))
+        }
+    }
+}
+
+
+export default connect(mapStateToProps, mapStateToDispatch)(Users)
