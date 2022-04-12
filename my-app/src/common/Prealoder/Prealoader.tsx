49c917bc7f2588a574fe30445cc6c2365cc2a5f3
Vlad Kravets <hazardvlad19@gmail.com>
2022-04-12 10:44:01 +0300

    add prealoder img

---
+++ my-app/src/common/Prealoder/Prealoader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import prealoader from '../../assets/images/prealoader.svg'
+
+
+export const Prealoader = () => {
+    return (
+        <div>
+            <img src={prealoader} alt="loading"/>
+        </div>
+    );
+};
+
