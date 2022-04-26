56da78875d3453e849ae0bfc3f1c859493c0f406
Vlad Kravets <hazardvlad19@gmail.com>
2022-04-26 14:19:39 +0300

    changes in News component

---
+++ my-app/src/components/News/News.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './News.module.css'
+export const News = () => {
+    return (
+        <div>
+            <img className={s.pageNews} src="https://englex.ru/app/uploads/5-best-websites-for-watching-news.png" alt="news"/>
+        </div>
+    );
+};
+
