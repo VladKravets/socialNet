ae20c714304afda1efb31c269cd1ff19e48236ac
Vlad Kravets <hazardvlad19@gmail.com>
2022-05-03 16:05:06 +0300

    added new Component

--- my-app/src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ my-app/src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './ProfileInfo.module.css'
 import {Prealoader} from "../../../common/Prealoder/Prealoader";
 import {ProfileResponseType} from "../../../Redux/profile-reducer";
-
+import {ProfileStatus} from "./ProfileStatus"
 export type ProfileInfoType = {
     profile: ProfileResponseType | null
 
@@ -14,16 +14,16 @@ const ProfileInfo: React.FC<ProfileInfoType> = (props) => {
     }
     return (
         <div>
-            <div>
-                <img
-                    src="https://media.istockphoto.com/photos/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-picture-id825383494?k=20&m=825383494&s=612x612&w=0&h=tEqZ5HFZcM3lmDm_cmI7hOeceiqy9gYrkyLTTkrXdY4="
-                    alt=""/>
-            </div>
+            {/*<div>*/}
+            {/*    <img*/}
+            {/*        src="https://media.istockphoto.com/photos/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-picture-id825383494?k=20&m=825383494&s=612x612&w=0&h=tEqZ5HFZcM3lmDm_cmI7hOeceiqy9gYrkyLTTkrXdY4="*/}
+            {/*        alt=""/>*/}
+            {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}alt={'user logo'}/>
+                <ProfileStatus status={'Hello my friends'}/>
                 <div>About me:{props.profile.aboutMe}</div>
                 <hr/>
-                ava+description
 
             </div>
         </div>
