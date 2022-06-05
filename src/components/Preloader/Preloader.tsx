5f6a8791bcd328f5e23fb87573998f1e35741791
Vlad Kravets <hazardvlad19@gmail.com>
2022-06-05 10:50:09 +0300

    added on gh-pages

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import s from './Preloader.module.css'
 
 export const Preloader = () => {
     return (
-        <div style={{boxShadow: 'inset 0 0 10px green', padding: 15, margin: 15}}>
-            ... LOADING ...
+        <div className={s.preloader}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
-    )
-}
+    );
+};
