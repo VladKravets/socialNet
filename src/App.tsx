cbb63b9ecf4a97c15be562478932a1bc01b4b874
Vlad Kravets <hazardvlad19@gmail.com>
2022-06-05 11:51:16 +0300

    any changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Navbar} from './Components/Navbar/Navbar';
 import {Content} from './Components/Content/Content';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 import HeaderContainer from './Components/Header/HeaderContainer';
 import {useDispatch, useSelector} from 'react-redux';
 import {initializeApp} from './redux/reducers/app-reducer';
@@ -25,7 +25,7 @@ export const App: React.FC<AppPropsType> = () => {
     if (!isInitialized) return <Preloader />
 
     return (
-        <BrowserRouter>
+        <HashRouter>
             <div className="App">
                 <HeaderContainer/>
                 <div className={'container'}>
@@ -33,6 +33,6 @@ export const App: React.FC<AppPropsType> = () => {
                     <Content />
                 </div>
             </div>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
