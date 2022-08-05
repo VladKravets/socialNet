daf816eae639457019a144dda65ed2ca6934473b
Vlad Kravets <hazardvlad19@gmail.com>
2022-08-05 15:50:06 +0300

    fixed styles different components and structure code into project

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import style from './Header.module.scss';
 import {Link} from 'react-router-dom';
 import {SButton} from '../SButton/SButton';
 import logo from "../../assets/forEach()-logos/forEach()-logos.jpeg"
+
 type HeaderType = {
     login: string | null
     isAuth: boolean
@@ -18,14 +19,13 @@ export const Header: React.FC<HeaderType> = ({isAuth, login, logout}) => {
     return (
         <header className={style.header}>
             <div className={'container'}>
-                    <img className={style.logo} src={logo} alt=""/>
+                <img className={style.logo} src={logo} alt=""/>
                 <div>
-                    {
-                        isAuth
-                            ? <>{` ${login} `}
-                                <SButton onClick={logoutHandle}>выйти</SButton>
-                            </>
-                            : <Link to={'/login'}>Login</Link>
+                    {isAuth
+                        ? <>{` ${login} `}
+                            <SButton onClick={logoutHandle}>выйти</SButton>
+                        </>
+                        : <Link to={'/login'}>Login</Link>
                     }
                 </div>
             </div>
